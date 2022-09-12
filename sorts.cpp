cf048435433dea42fca6968b76b847031af9443e
AceAtomz <93909209+AceAtomz@users.noreply.github.com>
2022-09-12 13:52:28 +0200

    bumb

--- sorts.cpp
+++ sorts.cpp
@@ -5,13 +5,21 @@
 using namespace std;
 
 vector<int> generateNums(int N){
+    vector<int> arr;
+
     random_device rd;
     mt19937 generator(rd());
-    uniform_int_distribution<> dist(1, 10);
-    int number = dist(generator); // a random number
-    cout << number << endl;
+    uniform_int_distribution<> dist(1, 1000000);
+    for(int i=0; i<N;i++){
+        arr.push_back(dist(generator));  // some random numbers
+        cout << arr[i] << endl;
+    }
+
+    return arr;
 }
 
-void main(){
-    vector<int> J = generateNums(5);
+int main(){
+    vector<int> nums = generateNums(5);
+    return 0;
 }
+
