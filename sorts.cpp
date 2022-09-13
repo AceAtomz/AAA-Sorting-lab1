07073bbd1b5bd537989b22a03c437a4342378e50
AceAtomz <93909209+AceAtomz@users.noreply.github.com>
2022-09-13 11:16:36 +0200

    job done

--- sorts.cpp
+++ sorts.cpp
@@ -12,16 +12,15 @@ int maxsize = 20000;
 vector<int> times;
 int runs = 5;
 
-vector<int> generateNums(int N){
-    vector<int> arr;
-
-    random_device rd;
+random_device rd;
     mt19937 generator(rd());
     uniform_int_distribution<> dist(1, 1000000);
+
+vector<int> generateNums(int N){
+    vector<int> arr;
     for(int i=0; i<N;i++){
         arr.push_back(dist(generator));  // some random numbers
     }
-
     return arr;
 }
 
@@ -96,18 +95,15 @@ vector<int> BubbleEscapeSort(vector<int> arr, int n)
    return arr;
 }
 
-void printSorts(vector<int> times){
+void printSorts(string SS, string BB, string BBE){
     ofstream fw("C:\\Users\\User\\Documents\\GitHub\\AAA-Sorting-lab1\\data.txt", ofstream::out);
 
     //check if file was successfully opened for writing
     if (fw.is_open())
     {
       //store array contents to text file
-      for (int i = 0; i < 20; i++) {
-        fw << times[i];
-        if(i!=19) fw << ",";
-        //else fw << "\n";
-      }
+      fw << SS << "\n" << BB << "\n" << BBE;
+
       fw.close();
     }
     else cout << "Problem with opening file";
@@ -116,27 +112,62 @@ void printSorts(vector<int> times){
 }
 
 int main(){
-    for(int n=size;n<maxsize;n+=size){
-        int total_time = 0;
-        for(int i=0;i<runs;i++){
-            vector<int> OGnums = generateNums(size);
+    string SS = "";
+    string BB = "";
+    string BBE = "";
 
+    for(int n=size;n<=maxsize;n+=size){
+        vector<int> vec1 = generateNums(n);
+        vector<int> vec2 = generateNums(n);
+        vector<int> vec3 = generateNums(n);
+        vector<int> vec4 = generateNums(n);
+        vector<int> vec5 = generateNums(n);
+
+        vector<vector<int>> vectors{vec1, vec2, vec3, vec4, vec5};
+
+        //SS
+        long long total_time = 0;
+        for(int i=0;i<runs;i++){
             auto start = chrono::high_resolution_clock::now();
-            vector<int> SSnums = SelectionSort(OGnums, size);
+            vector<int> SSnums = SelectionSort(vectors[i], n);
+
+            auto finish = chrono::high_resolution_clock::now();
+            total_time += chrono::duration_cast<chrono::nanoseconds>(finish-start).count();;
+        }
+        long long average_time = total_time / runs;
+        SS += to_string(average_time);
+        if(n!=maxsize) SS += ",";
+
+        //BB
+        total_time = 0;
+        for(int i=0;i<runs;i++){
+            auto start = chrono::high_resolution_clock::now();
+            vector<int> BBnums = BubbleSort(vectors[i], n);
 
             auto finish = chrono::high_resolution_clock::now();
             int time_elapsed = chrono::duration_cast<chrono::nanoseconds>(finish-start).count();
             total_time += time_elapsed;
         }
-        int average_time = total_time / runs;
-        times.push_back(average_time);
-        //cout << average_time << endl;
+        average_time = total_time / runs;
+        BB += to_string(average_time);
+        if(n!=maxsize) BB += ",";
+
+        //BBE
+        total_time = 0;
+        for(int i=0;i<runs;i++){
+            auto start = chrono::high_resolution_clock::now();
+            vector<int> BBEnums = BubbleEscapeSort(vectors[i], n);
+
+            auto finish = chrono::high_resolution_clock::now();
+            int time_elapsed = chrono::duration_cast<chrono::nanoseconds>(finish-start).count();
+            total_time += time_elapsed;
+        }
+        average_time = total_time / runs;
+        BBE += to_string(average_time);
+        if(n!=maxsize) BBE += ",";
     }
 
-    printSorts(times);
-
-    //vector<int> BSnums = BubbleSort(OGnums, size);
-    //vector<int> BESnums = BubbleEscapeSort(OGnums, size);
+    printSorts(SS, BB, BBE);
 
     return 0;
 }
